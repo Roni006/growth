f2f9c8f78a5cba0ca2ad6908fa9043458328d5ef
Anik Islam <anik54908@gmail.com>
2026-04-07 01:24:00 +0600

    project initialize done

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Container from "./components/Container"
+
+ 
+
+function App() { 
+
+  return (
+    <>
+    <Container>
+
+      <div>
+        
+      <p className="text-red-600 bg-green-600 text-5xl">
+        Click on the Vite and React logos to learn more
+      </p>
+      </div>
+    </Container>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Container.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Container = ({children}) => {
+  return (
+    <div className='max-w-[1330px] mx-auto px-3.75'>{children}</div>
+  )
+}
+
+export default Container
