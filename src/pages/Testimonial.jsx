e19539ddc2288f7259e3b89c5a24372847e38e9a
Anik Islam <anik54908@gmail.com>
2026-04-14 01:26:40 +0600

    creatd testtimonial components and data dynamically using map function

--- src/pages/Testimonial.jsx
+++ src/pages/Testimonial.jsx
@@ -1,9 +1,35 @@
 import Container from "../components/Container";
 
+import Testimonial1 from "../../src/img/testimonial/testi-1.png";
+import Testimonial2 from "../../src/img/testimonial/testi-2.png";
+import Testimonial3 from "../../src/img/testimonial/testi-3.png";
+
 const Testimonial = () => {
+  const testimonials = [
+    {
+      img: Testimonial1,
+      name: "Kyle Winder",
+      position: "Internal Data Facilitator",
+      feedback: "Lorem ipsum dolor sit amet, consectetur adipiscing ",
+    },
+
+    {
+      img: Testimonial2,
+      name: "Julio Hickle",
+      position: "District Web Administrator",
+      feedback: "Lorem ipsum dolor sit amet, consectetur adipiscing ",
+    },
+
+    {
+      img: Testimonial3,
+      name: "Lee Beahanr",
+      position: "Regional Web Planner",
+      feedback: "Lorem ipsum dolor sit amet, consectetur adipiscing ",
+    },
+  ];
   return (
     <>
-      <section className="py-10">
+      <section className="py-10 bg-[#EFEFEF]">
         <Container>
           <div>
             {/* top content  */}
@@ -17,6 +43,24 @@ const Testimonial = () => {
               </p>
             </div>
             {/* top content  */}
+
+            {/* testtmonial content  */}
+            <div className="mt-17.5 grid grid-cols-3 items-center justify-center gap-16">
+              {testimonials.map((item, i) => (
+                <div className="max-w-95 bg-[#552FFF] flex flex-col items-center justify-center text-center rounded-[20px] py-17.5 px-12.5">
+                  <img src={item.img} alt="Testimonial 1" />
+                  <h3 className="text-white font-normal text-[32px] leading-[130%] pt-5.5 pb-3 ">
+                    {item.name}
+                  </h3>
+                  <span className="font-normal text-[16px] text-white leading-[130%] ">
+                    {item.position}
+                  </span>
+                  <p className="font-normal text-white text-[20px] pt-7.5  ">
+                    {item.feedback}{" "}
+                  </p>
+                </div>
+              ))}
+            </div>
           </div>
         </Container>
       </section>
