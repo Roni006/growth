580e624f53ad6c0b39712f3ac9750e22ab889c82
Anik Islam <anik54908@gmail.com>
2026-04-14 20:08:27 +0600

    created blog components

--- src/pages/OurBlog.jsx
+++ src/pages/OurBlog.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Container from "../components/Container";
 
+import blog1 from '../../src/img/blog/blog1.png'
+import blog2 from '../../src/img/blog/blog2.png'
+import blog3 from '../../src/img/blog/blog3.png'
+
 const OurBlog = () => {
   return (
     <>
@@ -18,6 +22,38 @@ const OurBlog = () => {
               </p>
             </div>
             {/* top content  */}
+
+            {/* blog content  */}
+            <div className="mt-[70px] grid grid-cols-3 items-center justify-center gap-[36px] ">
+
+              <div className="w-[410px]">
+                <div className="relative">
+                  <span className="absolute top-3 left-3 font-normal font-overpass text-[16px] bg-[#552fff] text-white py-4 px-11.5 rounded-[15px] ">Insight</span>
+                  <img className="w-full rounded-md" src={blog1} alt="" />
+                </div>
+                <p className="text-[18px] font-normal text-[#222222] font-poppins p-[10px_0_20px_0] ">Sat Feb 18, 2023</p>
+                <h3 className="text-[#552fff] font-semibold font-overpass text-[32px] leading-[130%] max-w-[310px] ">Navigating the Path to Prosperity</h3>
+              </div>
+
+              <div className="w-[410px]">
+                <div className="relative">
+                  <span className="absolute top-3 left-3 font-normal font-overpass text-[16px] bg-[#552fff] text-white py-4 px-11.5 rounded-[15px] ">Insight</span>
+                  <img className="w-full rounded-md" src={blog1} alt="" />
+                </div>
+                <p className="text-[18px] font-normal text-[#222222] font-poppins p-[10px_0_20px_0] ">Sat Feb 18, 2023</p>
+                <h3 className="text-[#552fff] font-semibold font-overpass text-[32px] leading-[130%] max-w-[310px] ">Navigating the Path to Prosperity</h3>
+              </div>
+              
+              <div className="w-[410px]">
+                <div className="relative">
+                  <span className="absolute top-3 left-3 font-normal font-overpass text-[16px] bg-[#552fff] text-white py-4 px-11.5 rounded-[15px] ">Insight</span>
+                  <img className="w-full rounded-md" src={blog1} alt="" />
+                </div>
+                <p className="text-[18px] font-normal text-[#222222] font-poppins p-[10px_0_20px_0] ">Sat Feb 18, 2023</p>
+                <h3 className="text-[#552fff] font-semibold font-overpass text-[32px] leading-[130%] max-w-[310px] ">Navigating the Path to Prosperity</h3>
+              </div>
+            </div>
+            {/* blog content  */}
           </div>
         </Container>
       </section>
@@ -26,3 +62,4 @@ const OurBlog = () => {
 };
 
 export default OurBlog;
+
